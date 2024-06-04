8ccef9c9c56d6522e3b124f0e1266afd82758dfc
Pavel Asafov <pavelaof@gmail.com>
2024-06-04 15:53:44 +0300

    add coin by time

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const tap = document.querySelector('.tap')
 const amount = document.querySelector('.amount')
-const increaseByTapNumber = document.querySelector('.increase_by_tap_number')
+const increaseByTap = document.querySelector('.increase_by_tap')
+const increaseByTime = document.querySelector('.increase_by_time')
 const energyEl = document.querySelector('.energy')
 const tg = window.Telegram.WebApp
 const coin = {
   amount: 0,
   inceaseByTap: 3,
+  increaseByHour: 3600,
+  increaseBySecond: 3600 / 3600,
+  delayIncrease: 3600 / 3600 * 1000,
 }
 const energy = {
   decrease: 1,
   current: 2000,
   total: 2000,
+  increase: 3,
+  increaseDelay: 1000,
 }
+
+tg.expand()
 
 // Добавить имя
 const user = document.querySelector('.user')
@@ -24,10 +32,13 @@ const userName = firstName + ' ' + lastName
 user.textContent = userName
 // ------------------------
 // Установить значение увеличения по клику
-increaseByTapNumber.textContent = '+' + coin.inceaseByTap
+increaseByTap.textContent = '+' + coin.inceaseByTap
 // ------------------------
 // Установить энергию
 energyEl.textContent = `${energy.current} / ${energy.total}`
+// ------------------------
+// Установить увеличение монет по времени
+increaseByTime.textContent = '+' + coin.increaseByHour
 // ------------------------
 // Уменьшить энергию
 function energise() {
@@ -44,3 +55,26 @@ function increaseCoinByTap() {
 }
 
 tap.addEventListener('click', increaseCoinByTap)
+
+// Увеличить энергию
+function increaseEnergy() {
+  if (energy.current < energy.total) {
+    energy.current += energy.increase
+
+    if (energy.current > energy.total) {
+      energy.current = energy.total
+    }
+  }
+
+  energyEl.textContent = `${energy.current} / ${energy.total}`
+}
+
+setInterval(increaseEnergy, energy.increaseDelay)
+
+// Увеличить монеты по времени
+function increaseCoinByTime() {
+  coin.amount += coin.increaseBySecond
+  amount.textContent = coin.amount
+}
+
+setInterval(increaseCoinByTime, coin.delayIncrease)
