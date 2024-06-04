ccf3ad6f1b68d9e9025151efc198e55e9bb92210
Pavel Asafov <pavelaof@gmail.com>
2024-06-04 17:22:41 +0300

    add second menu

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const amount = document.querySelector('.amount')
 const increaseByTap = document.querySelector('.increase_by_tap')
 const increaseByTime = document.querySelector('.increase_by_time')
 const energyEl = document.querySelector('.energy')
+const main = document.querySelector('.main')
+const menu2 = document.querySelector('.menu2')
+const menuBtn1 = document.querySelector('.menu_btn_1')
+const menuBtn2 = document.querySelector('.menu_btn_2')
 const tg = window.Telegram.WebApp
 const coin = {
   amount: 0,
@@ -20,6 +24,8 @@ const energy = {
 }
 
 tg.expand()
+
+menuBtn1.classList.add('menu_btn_active')
 
 // Добавить имя
 const user = document.querySelector('.user')
@@ -78,3 +84,24 @@ function increaseCoinByTime() {
 }
 
 setInterval(increaseCoinByTime, coin.delayIncrease)
+
+// Переключиться на меню 2
+
+// menuBtn1
+// menuBtn2
+
+menuBtn2.addEventListener('click', () => {
+  main.style.display = 'none'
+  menu2.style.display = 'flex'
+  menuBtn1.classList.remove('menu_btn_active')
+  menuBtn2.classList.add('menu_btn_active')
+  const profit = document.querySelector('.menu2_profit_item_number')
+  profit.textContent = coin.increaseByHour
+})
+
+menuBtn1.addEventListener('click', () => {
+  main.style.display = 'flex'
+  menu2.style.display = 'none'
+  menuBtn2.classList.remove('menu_btn_active')
+  menuBtn1.classList.add('menu_btn_active')
+})
